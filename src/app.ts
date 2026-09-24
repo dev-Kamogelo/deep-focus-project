c364aee4bab902551791b0fb4f59e957ab1b9464
Kamogelo <devkamogelo9467@gmail.com>
2026-09-24 14:30:36 +0200

    feat: add pause & reset functions

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
-import './style.css'
+import './style.css';
 
 const timeDisplay = document.querySelector("#time-display") as HTMLElement;
 const startSessionBtn = document.querySelector("#start-session") as HTMLButtonElement;
+const pauseSessionBtn = document.querySelector('#pause-session') as HTMLButtonElement;
+const resetSessionBtn = document.querySelector('#reset-session') as HTMLButtonElement;
 
-let intialTime: number = 1;
-let remainingTime = intialTime * 60;
+let inputTime: number = 1;
+let initialTime = inputTime * 60;
+let remainingTime = initialTime ;
 let countDown: ReturnType<typeof setInterval>;
 
 function renderTimeDisplay() {
@@ -15,10 +18,10 @@ function renderTimeDisplay() {
     const diplaySec = second.toString().padStart(2, '0');
 
     timeDisplay.innerHTML = `${diplayMin}:${diplaySec}`;
-    console.log(diplayMin, diplaySec);
-}
+};
 
 function startSession() {
+
     clearInterval(countDown);
     
     countDown = setInterval(() => {
@@ -27,12 +30,34 @@ function startSession() {
 
         if(remainingTime <= 0) {
             clearInterval(countDown);
-            console.log('time done')
-        }
+            console.log('time done');
+        };
+
     }, 1000);
 
     startSessionBtn.disabled = true;
+    pauseSessionBtn.disabled = false
+
+};
+
+function pauseSession() {
+    startSessionBtn.disabled = false;
+
+    clearInterval(countDown);
+    remainingTime = remainingTime;
+    renderTimeDisplay();
+};
+
+function resetSession() {
+    startSessionBtn.disabled = false;
+    pauseSessionBtn.disabled = true
+
+    clearInterval(countDown);
+    remainingTime = initialTime;
+    renderTimeDisplay()
 }
 
 startSessionBtn.addEventListener('click', startSession);
+pauseSessionBtn.addEventListener('click', pauseSession);
+resetSessionBtn.addEventListener('click', resetSession);
 window.addEventListener('load', renderTimeDisplay);
